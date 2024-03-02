5b0aec17ff9d50e7d09bc40d117d9da77b183721
nayeem <nayeemahammed853@gmail.com>
2024-03-03 02:52:45 +0600

    modified: src/auth/controllers/user.controller.ts and  src/auth/services/user.service.ts

--- src/auth/controllers/user.controller.ts
+++ src/auth/controllers/user.controller.ts
@@ -1,66 +1,96 @@
-import { Controller, Request,Post, UploadedFile, UseGuards, UseInterceptors, Get, Res, Param } from '@nestjs/common';
+import { Controller, Request, Post, UploadedFile, UseGuards, UseInterceptors, Get, Res, Param, Put, Body } from '@nestjs/common';
 import { UserService } from '../services/user.service';
 import { JwtGuard } from '../guards/jwt.guard';
 import { FileInterceptor } from '@nestjs/platform-express';
-import {  isFileExtensionSafe, removeFile, saveImageToStorages } from '../helpers/image-storage';
+import { isFileExtensionSafe, removeFile, saveImageToStorages } from '../helpers/image-storage';
 import { Observable, of, switchMap } from 'rxjs';
 import { join } from 'path';
 import { User } from '../models/user.interface';
-import { FriendRequest } from '../models/friend.request.interface';
+import { FriendRequest, FriendRequestStatus, FriendRequest_Status } from '../models/friend.request.interface';
 
 @Controller('user')
 export class UserController {
-    constructor(private userService: UserService) {}
+  constructor(private userService: UserService) { }
 
-    @UseGuards(JwtGuard)
-    @Post('upload')
-    @UseInterceptors(FileInterceptor('file', saveImageToStorages))
-    uploadImage(@UploadedFile() file: Express.Multer.File, @Request() req): any {
-        const filename = file?.filename;
+  @UseGuards(JwtGuard)
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file', saveImageToStorages))
+  uploadImage(@UploadedFile() file: Express.Multer.File, @Request() req): any {
+    const filename = file?.filename;
 
-        if(!filename) return of({error: 'File must be a png. jpg/jpeg'});
+    if (!filename) return of({ error: 'File must be a png. jpg/jpeg' });
 
-        const imageFolderPath = join(process.cwd(),'images');
-        const fullImagePath = join(imageFolderPath +'/'+ file.filename);
+    const imageFolderPath = join(process.cwd(), 'images');
+    const fullImagePath = join(imageFolderPath + '/' + file.filename);
 
-        return isFileExtensionSafe(fullImagePath).pipe(
-            switchMap((isFileLegit: boolean) => {
-                if(isFileLegit) {
-                    const userId = req.user.id;
-                    return this.userService.updateUserImageById(userId, filename);
-                }
-                removeFile(fullImagePath)
-        return of({error: "File content does not match extention"});
-            })
-        )
-        
-    }
+    return isFileExtensionSafe(fullImagePath).pipe(
+      switchMap((isFileLegit: boolean) => {
+        if (isFileLegit) {
+          const userId = req.user.id;
+          return this.userService.updateUserImageById(userId, filename);
+        }
+        removeFile(fullImagePath)
+        return of({ error: "File content does not match extention" });
+      })
+    )
 
-    @UseGuards(JwtGuard)
-    @Get('image')
-    findImage(@Request() req, @Res() res): Observable<Object> {
-      const userId = req.user.id;
-      return this.userService.findImageNameByUserId(userId).pipe(
-        switchMap((imageName: string) => {
-          return of(res.sendFile(imageName, { root: './images' }));
-        }),
-      );
-    }
+  }
 
-    @UseGuards(JwtGuard)
-    @Get(':userId')
-    findUserById(@Param('userId') userStringId: string): Observable<User> {
-      const userId = parseInt(userStringId);
-      return this.userService.findUserById(userId);
-    }
+  @UseGuards(JwtGuard)
+  @Get('image')
+  findImage(@Request() req, @Res() res): Observable<Object> {
+    const userId = req.user.id;
+    return this.userService.findImageNameByUserId(userId).pipe(
+      switchMap((imageName: string) => {
+        return of(res.sendFile(imageName, { root: './images' }));
+      }),
+    );
+  }
 
-    @UseGuards(JwtGuard)
-    @Post('friend-request/send/:receiverId')
-    sendFriendRequest(
-      @Request() req,
-      @Param('receiverId') receiverStringId: string,
-    ): Observable<FriendRequest | {error: string}> {
-      const receiverId = parseInt(receiverStringId);
-      return this.userService.sendFriendRequest(receiverId, req.user);
-    }
+  @UseGuards(JwtGuard)
+  @Get(':userId')
+  findUserById(@Param('userId') userStringId: string): Observable<User> {
+    const userId = parseInt(userStringId);
+    return this.userService.findUserById(userId);
+  }
+
+  @UseGuards(JwtGuard)
+  @Post('friend-request/send/:receiverId')
+  sendFriendRequest(
+    @Request() req,
+    @Param('receiverId') receiverStringId: string,
+  ): Observable<FriendRequest | { error: string }> {
+    const receiverId = parseInt(receiverStringId);
+    return this.userService.sendFriendRequest(receiverId, req.user);
+  }
+
+  @UseGuards(JwtGuard)
+  @Put('friend-request/response/:friendRequestId')
+  responseToFriendRequest(
+    @Body() statusResponse: FriendRequestStatus,
+    @Param('receiverId') friendRequestStringId: string
+  ): Observable<FriendRequestStatus> {
+    const friendRequestId = parseInt(friendRequestStringId);
+    return this.userService.responseToFriendRequest(statusResponse, friendRequestId);
+  }
+
+  @UseGuards(JwtGuard)
+  @Get('friend-request/status/:receiverId')
+  getFriendRequestStatus(
+    @Request() req: any,
+    @Param('receiverId') receiverStringId: string
+  ): Observable<FriendRequestStatus> {
+    const receiverId = parseInt(receiverStringId);
+    return this.userService.getFriendRequestStatus(receiverId, req.user);
+  }
+
+
+  @UseGuards(JwtGuard)
+  @Get('friend-request/me/receiver')
+  getFriendRequestsFromRecipients(
+    @Request() req,
+  ): Observable<FriendRequest []> {
+    return this.userService.getFriendRequestsFromRecipients(req.user);
+  }
+
 }
--- src/auth/services/user.service.ts
+++ src/auth/services/user.service.ts
@@ -5,7 +5,7 @@ import { UserEntity } from '../models/user.entities';
 import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository, UpdateResult } from 'typeorm';
-import { FriendRequest } from '../models/friend.request.interface';
+import { FriendRequest, FriendRequestStatus } from '../models/friend.request.interface';
 import { FriendRequestEntity } from '../models/friend.request.entity';
 
 @Injectable()
@@ -14,14 +14,14 @@ export class UserService {
     constructor(
         @InjectRepository(UserEntity) private userRepo: Repository<UserEntity>,
         @InjectRepository(FriendRequestEntity) private friendRequestRepositoy: Repository<FriendRequestEntity>,
-        ) {}
+    ) { }
 
     findUserById(id: number): Observable<User> {
 
 
         return from(
             this.userRepo.findOne({
-                where:{id},
+                where: { id },
                 relations: ['feedPosts']
             })
         ).pipe(
@@ -32,7 +32,7 @@ export class UserService {
         )
     }
 
-    updateUserImageById(id: number, imagePath: string): Observable<UpdateResult>{
+    updateUserImageById(id: number, imagePath: string): Observable<UpdateResult> {
         const user: User = new UserEntity();
 
         user.id = id;
@@ -41,7 +41,7 @@ export class UserService {
     }
 
     findImageNameByUserId(id: number): Observable<string> {
-        return from(this.userRepo.findOne({where:{id}})).pipe(
+        return from(this.userRepo.findOne({ where: { id } })).pipe(
             map((user: User) => {
                 delete user.password;
                 return user.imagePath;
@@ -51,40 +51,87 @@ export class UserService {
     hasRequestBeenSentOrReceived(
         creator: User,
         receiver: User,
-      ): Observable<boolean> {
+    ): Observable<boolean> {
         return from(
-          this.friendRequestRepositoy.findOne({
-            where: [
-              { creator, receiver },
-              { creator: receiver, receiver: creator },
-            ],
-          }),
+            this.friendRequestRepositoy.findOne({
+                where: [
+                    { creator, receiver },
+                    { creator: receiver, receiver: creator },
+                ],
+            }),
         ).pipe(
-          switchMap((friendRequest: FriendRequest) => {
-            if (!friendRequest) return of(false);
-            return of(true);
-          }),
+            switchMap((friendRequest: FriendRequest) => {
+                if (!friendRequest) return of(false);
+                return of(true);
+            }),
         );
-      }
+    }
 
-    sendFriendRequest(receiverId: number, creator: User): Observable<FriendRequest | {error: string}> {
-        if(receiverId === creator.id) return of({error:"invalid credential"});
-        
-        return  this.findUserById(receiverId).pipe(
+    sendFriendRequest(receiverId: number, creator: User): Observable<FriendRequest | { error: string }> {
+        if (receiverId === creator.id) return of({ error: "invalid credential" });
+
+        return this.findUserById(receiverId).pipe(
             switchMap((receiver: User) => {
                 return this.hasRequestBeenSentOrReceived(creator, receiver).pipe(
-                    switchMap((hasRequestBeenSentOrReceived: boolean) =>{
-                        if(hasRequestBeenSentOrReceived) return of({error: "Friend request has been already been send"})
+                    switchMap((hasRequestBeenSentOrReceived: boolean) => {
+                        if (hasRequestBeenSentOrReceived) return of({ error: "Friend request has been already been send" })
 
                         let friendRequest: FriendRequest = {
                             creator,
                             receiver,
                             status: 'pending',
                         }
-                        return from(this.friendRequestRepositoy.save(friendRequest));
+                        return from(this.friendRequestRepositoy.save(friendRequest,));
                     })
                 )
             })
         )
+    }
+
+    getFriendRequestStatus(
+        receiverId: number,
+        currentUser: User
+    ): Observable<FriendRequestStatus> {
+        return this.findUserById(receiverId).pipe(
+            switchMap((receiver: User) => {
+                return from(this.friendRequestRepositoy.findOne({
+                    where: [
+                        { creator: currentUser },
+                        { receiver: receiver }
+                    ]
+                }))
+            }),
+
+            switchMap((friendRequest: FriendRequest) => {
+                return of({ status: friendRequest.status })
+            })
+        )
+    }
+
+    getUserFriendRequestUserById( friendRequestId: number): Observable<FriendRequest> {
+        return from(this.friendRequestRepositoy.findOne({
+            where: [{id:friendRequestId}]
+        }))
+    }
+
+    responseToFriendRequest(
+        statusResponse: FriendRequestStatus,
+        friendRequestId: number
+
+    ): Observable<FriendRequestStatus> {
+        return this.getUserFriendRequestUserById(friendRequestId).pipe(
+            switchMap((friendRequest: FriendRequest) => {
+                return from(this.friendRequestRepositoy.save({
+                    ...friendRequest,
+                    status: statusResponse.status,
+                }))
+            })
+        )
+    }
+
+    getFriendRequestsFromRecipients(currentUser: User): Observable<FriendRequest []> {
+        return from(this.friendRequestRepositoy.find({
+            where: [{receiver: currentUser}]
+        }))
     }
 }
